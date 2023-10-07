5e3720d8867a90289373ad0ec1c00885102702f2
wansui <wanjinhang@gmail.com>
2023-10-07 21:40:13 +0800

    初始化

---
+++ src/test/java/com/wansui/springbootjavaweb/SpringBootJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wansui.springbootjavaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
